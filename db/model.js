cdbd428bb13d516d6bc256fb37041545ad18ad23
Andykingche <laqm_14@hotmail.com>
2021-01-07 18:33:47 -0500

    add to API-GraphQL : create and update

--- db/model.js
+++ db/model.js
@@ -13,20 +13,77 @@ module.exports ={
               },{
                 timestamps: false
             })
+            
             return User
       
         },
-        Categoria: async()=>{
+        Categoria: async(db)=>{
             let Category = db.define("categorias",{
                 nombre_categoria:DataTypes.TEXT
+            },{
+                timestamps: false
             })
             return Category
         },
-        Etiqueta:async()=>{
+        Etiqueta:async(db)=>{
             let Tags = db.define("etiquetas",{
                 nombre_etiqueta:DataTypes.TEXT
+            },{
+                timestamps: false
             })
-        }
+            return Tags
+        },
+        Comentario:async(db)=>{
+            let Comment = db.define("comentarios",{
+                contenido:DataTypes.TEXT,
+                usuariosid: {
+                    type: DataTypes.INTEGER,
+                    foreignKey: true
+                  }
+            },{
+                timestamps: false
+            })
+            
+            return Comment
+
+        },
+        Post: async (db) => {
+            let Post = db.define("posts",{
+                categoriasid: {
+                    type: DataTypes.INTEGER,
+                    foreignKey: true
+                  },comentarioid: {
+                    type: DataTypes.INTEGER,
+                    foreignKey: true
+                  },usuariosid: {
+                    type: DataTypes.INTEGER,
+                    foreignKey: true
+                  },
+                  titulo: DataTypes.TEXT,
+                  fecha_publicacion: DataTypes.DATE,
+                  estado: DataTypes.BOOLEAN
+
+            },{
+                timestamps: false
+            })
+
+            return Post
+
+        },
+        PostEtiqueta: async (db) => {
+            let PostEtiqueta = db.define("posts_etiquetas",{
+                idetiqueta:{
+                    type: DataTypes.INTEGER,
+                    foreignKey: true
+                },
+                idpost:{
+                    type: DataTypes.INTEGER,
+                    foreignKey: true
+                }
+            },{ timestamps: false })
+
+            return PostEtiqueta
+        } 
 
 } 
 
