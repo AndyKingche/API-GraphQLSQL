47b8b01a450d3be776a23536e95262b37ecba85b
Andykingche <laqm_14@hotmail.com>
2021-03-04 00:17:47 -0500

    fix queries and types

--- db/model.js
+++ db/model.js
@@ -3,28 +3,17 @@ const { DataTypes } = require("sequelize");
 
 module.exports ={
     Usuario: async(db) =>{
-            let User = db.define("usuarios", {
-                
+            let User = db.define("usuarios", {  
                 nombre: DataTypes.TEXT,
                 email:  DataTypes.TEXT,
                 nickname: DataTypes.TEXT,
                 login: DataTypes.TEXT,
-                password:DataTypes.TEXT
+                password:DataTypes.TEXT,
+                genero:DataTypes.TEXT
               },{
                 timestamps: false
             })
-            let Comment = db.define("comentarios",{
-                contenido:DataTypes.TEXT,
-                usuariosid: {
-                    type: DataTypes.INTEGER,
-                    foreignKey: true,
-                    
-                  }
-            },{
-                timestamps: false
-            })
             
-            User.hasOne(Comment, { foreignKey: 'usuariosid' });
 
             return User
       
@@ -36,35 +25,31 @@ module.exports ={
                 timestamps: false
             })
             return Category
-        },
-        Etiqueta:async(db)=>{
-            let Tags = db.define("etiquetas",{
-                nombre_etiqueta:DataTypes.TEXT
-            },{
-                timestamps: false
-            })
-            return Tags
-        },
+        }
+        // ,Etiqueta:async(db)=>{
+        //     let Tags = db.define("etiquetas",{
+        //         nombre_etiqueta:DataTypes.TEXT
+        //     },{
+        //         timestamps: false
+        //     })
+        //     return Tags
+        // }
+        ,
         Comentario:async(db)=>{
             let Comment = db.define("comentarios",{
                 contenido:DataTypes.TEXT,
                 usuariosid: {
                     type: DataTypes.INTEGER,
                     foreignKey: true,                 
-                  }
+                  },
+                postsid:{
+                    type: DataTypes.INTEGER,
+                    foreignKey: true,  
+                }
             },{
                 timestamps: false
             })
-            let User = db.define("usuarios", {
-                nombre: DataTypes.TEXT,
-                email:  DataTypes.TEXT,
-                nickname: DataTypes.TEXT,
-                login: DataTypes.TEXT,
-                password:DataTypes.TEXT
-              },{
-                timestamps: false
-            })
-            Comment.belongsTo(User, { foreignKey: 'usuariosid' });
+            
             return Comment
 
         },
@@ -73,15 +58,14 @@ module.exports ={
                 categoriasid: {
                     type: DataTypes.INTEGER,
                     foreignKey: true
-                  },comentarioid: {
-                    type: DataTypes.INTEGER,
-                    foreignKey: true
                   },usuariosid: {
                     type: DataTypes.INTEGER,
                     foreignKey: true
                   },
                   titulo: DataTypes.TEXT,
                   fecha_publicacion: DataTypes.DATE,
+                  contenido:DataTypes.TEXT,
+                  keywords: DataTypes.TEXT,
                   estado: DataTypes.BOOLEAN
 
             },{
@@ -90,21 +74,21 @@ module.exports ={
 
             return Post
 
-        },
-        PostEtiqueta: async (db) => {
-            let PostEtiqueta = db.define("posts_etiquetas",{
-                idetiqueta:{
-                    type: DataTypes.INTEGER,
-                    foreignKey: true
-                },
-                idpost:{
-                    type: DataTypes.INTEGER,
-                    foreignKey: true
-                }
-            },{ timestamps: false })
+        }
+        // ,PostEtiqueta: async (db) => {
+        //     let PostEtiqueta = db.define("posts_etiquetas",{
+        //         idetiqueta:{
+        //             type: DataTypes.INTEGER,
+        //             foreignKey: true
+        //         },
+        //         idpost:{
+        //             type: DataTypes.INTEGER,
+        //             foreignKey: true
+        //         }
+        //     },{ timestamps: false })
 
-            return PostEtiqueta
-        } 
+        //     return PostEtiqueta
+        // } 
 
 } 
 
