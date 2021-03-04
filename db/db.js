80c79046ce5c0c240359561aadd09c5c19ed8ec1
Andykingche <laqm_14@hotmail.com>
2021-03-04 15:59:32 -0500

    reestructured directories

--- db/db.js
+++ db/db.js
@@ -1,17 +1,24 @@
+'use strict'
 const { Sequelize } = require('sequelize')
 const model = require('../db/model')
-
 const errores = require('../lib/errors')
-const { //aqui ubicamos las variables que se creo en el archivo donde se puso las variables de entorno
-    DB_USER,
-    DB_PASSWD,
-    DB_HOST,
-    DB_NAME,
-    DB_DBMS,
-    DB_PORT
-    }=process.env
 
-const sequelize = new Sequelize(`${DB_NAME}`,`${DB_USER}`,`${DB_PASSWD}`,{
+let id = 0
+const { //aqui ubicamos las variables que se creo en el archivo donde se puso las variables de entorno
+  DB_USER,
+  DB_PASSWD,
+  DB_HOST,
+  DB_NAME,
+  DB_DBMS,
+  DB_PORT,
+  DBsql_USER,
+  DBsql_PASSWD,
+  DBsql_HOST,
+  DBsql_NAME,
+  DBsql_DBMS,
+  DBsql_PORT
+  }=process.env
+ let sequelize = new Sequelize(`${DB_NAME}`,`${DB_USER}`,`${DB_PASSWD}`,{
     host:`${DB_HOST}`,
     dialect:`${DB_DBMS}`,
     port:`${DB_PORT}`,
@@ -28,11 +35,49 @@ const sequelize = new Sequelize(`${DB_NAME}`,`${DB_USER}`,`${DB_PASSWD}`,{
       require:1000000
     },
     logging:false
-})
+  })
 
-function connectDB(){
-    
+// function connectDB(){
+//     console.log("id es", id)
+//     try {
+//        sequelize.authenticate();
+//         const models={
+//           usuarios: model.Usuario(sequelize),
+//           comentarios:model.Comentario(sequelize),
+//           categorias:model.Categoria(sequelize),
+//           posts:model.Post(sequelize)
+//         }
+        
+//         Object.keys(models).forEach(modelName => {
+//           if ('associate' in models[modelName]) {
+//             models[modelName].associate(models)
+//           }
+//         })
+        
+//         console.log('La conexion se ha establecido con exito');
+//         models.sequelize = sequelize
+//         models.Sequelize = Sequelize      
+//         return models
+//       } catch (error) {
+          
+//         console.error('Imposible conectar con la base de datos:');
+//         errores(error)
+//       }
+// }
+// function chosseDataBase(id){
+//   console.log("no paso por aqui")
+//     id = id
+//   connectDB()
+//   }
+//   escogerbasedatos()
+// module.exports = connectDB;
+// module.exports = chosseDataBase;
+
+module.exports={
+  
+  connectDB:()=>{
     try {
+      
        sequelize.authenticate();
         const models={
           usuarios: model.Usuario(sequelize),
@@ -47,7 +92,7 @@ function connectDB(){
           }
         })
         
-        console.log('La conexion se ha establecido con exito');
+        console.log('La conexion se ha establecido con exito',sequelize.options.dialect);
         models.sequelize = sequelize
         models.Sequelize = Sequelize      
         return models
@@ -56,5 +101,52 @@ function connectDB(){
         console.error('Imposible conectar con la base de datos:');
         errores(error)
       }
+  },
+  connectDBmssql:()=>{
+    let sequelize2 = new Sequelize(`${DBsql_NAME}`,`${DBsql_USER}`,`${DBsql_PASSWD}`,{
+      host:`${DBsql_HOST}`,
+      dialect:`${DBsql_DBMS}`,
+      port:`${DBsql_PORT}`,
+      define: {},
+      dialectOptions: {
+        options: {
+          requestTimeout: 60000000
+        }
+      },
+      pool: {
+        max: 10,
+        min: 0,
+        idle: 100000,
+        require:1000000
+      },
+      logging:false
+    })
+    console.log("entre")
+    try {
+      
+       sequelize2.authenticate();
+    
+        const models={
+          usuarios: model.Usuario(sequelize2),
+          comentarios:model.Comentario(sequelize2),
+          categorias:model.Categoria(sequelize2),
+          posts:model.Post(sequelize2)
+        }
+        Object.keys(models).forEach(modelName => {
+          if ('associate' in models[modelName]) {
+            models[modelName].associate(models)
+          }
+        })
+        
+        console.log('La conexion se ha establecido con exito',sequelize2.options.dialect);
+        models.sequelize2 = sequelize2
+        models.Sequelize = Sequelize    
+        return models
+      } catch (error) {
+          
+        console.error('Imposible conectar con la base de datos:');
+        errores(error)
+      }
+  }
+  
 }
-module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,35 +1,52 @@
 'use strict'
 require('dotenv').config();
-
-const connectDB = require('./db/db')
+const {connectDB,connectDBmssql} = require('./db/db')
 const { makeExecutableSchema } = require('graphql-tools')
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 const { readFile, readFileSync} = require('fs')
 const { join } = require('path')
 const resolvers = require('./lib/resolvers')
+const resolversmmsql= require('./lib/mssql/resolversmssql')
 const cors = require('cors');
 const { resolveCname } = require('dns');
 const app = express()
+const app1 = express()
 const port = process.env.port || 4000
+const port1 = process.env.port || 4001
+
 const typeDefs = readFileSync(
     join(
         __dirname, 'lib','schema.graphql'),'utf-8')
-
         
-const schema = makeExecutableSchema({typeDefs,resolvers})
-
+const schemamysql = makeExecutableSchema({typeDefs,resolvers})
+const schemamssql = makeExecutableSchema({typeDefs,resolversmmsql})
 app.use(cors())
-app.use('/api',graphqlHTTP({
-    schema: schema,
+app1.use(cors())
+app.use('/api/mysql',graphqlHTTP({
+    schema: schemamysql,
     rootValue:resolvers,
     graphiql:true
 }))
+app1.use('/api/mssql',graphqlHTTP({
+    schema: schemamssql,
+    rootValue:resolversmmsql,
+    graphiql:true
+}))
 const models = connectDB()
+const modelsql = connectDBmssql()
 
-models.sequelize.sync().then(()=>{
-    app.listen(port, ()=>{
-        console.log(`EL SERVIDOR SE ESTA EJECUTANDO EN : http://localhost:${port}/api`)
+
+    app.listen(port,()=>{ 
+           console.log(`EL SERVIDOR SE ESTA EJECUTANDO EN : http://localhost:${port}/api/mysql`)    
+   })
+
+
+
+    app1.listen(port1,()=>{ 
+        console.log(`EL SERVIDOR SE ESTA EJECUTANDO EN : http://localhost:${port1}/api/mssql`)
     })
 
-})
+
+
+
